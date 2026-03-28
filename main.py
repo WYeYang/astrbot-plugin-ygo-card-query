bb0362f15d629258b31b2b33a8db8fc88d19bcc1
WYeYang <770138859@qq.com>
2026-03-29 01:31:14 +0800

    Fix get_plugin to return class instead of instance

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-游戏王自然语言查卡插件 - 基于asrtbot
+游戏王查卡插件 - 基于asrtbot
 """
 
 from .card_query import CardQueryPlugin
 
 
 def get_plugin():
-    return CardQueryPlugin()
+    return CardQueryPlugin
