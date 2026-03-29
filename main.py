e9083f594bf90a80e33b78da9824a7b27946271f
WYeYang <770138859@qq.com>
2026-03-29 15:03:59 +0800

    Add detailed monster subtypes and only show monster stats for monster cards

--- main.py
+++ main.py
@@ -146,19 +146,24 @@ class CardQueryPlugin(Star):
             # 构建回复消息
             response = f"🔍 查询结果: {first_card['name']}\n"
             response += f"类型: {first_card['type']}\n"
-            if "attribute" in first_card:
-                response += f"属性: {first_card['attribute']}\n"
-            if "attack" in first_card:
-                response += f"攻击力: {first_card['attack']}\n"
-            if "defense" in first_card:
-                response += f"防御力: {first_card['defense']}\n"
-            # 根据卡片类型显示相应的字段
-            if "link" in first_card:
-                response += f"链接: {first_card['link']}\n"
-            elif "rank" in first_card:
-                response += f"阶级: {first_card['rank']}\n"
-            elif "level" in first_card:
-                response += f"等级: {first_card['level']}\n"
+            
+            # 只有怪兽卡才显示属性、攻击、防御、等级等信息
+            is_monster = "怪兽" in first_card.get('type', '')
+            if is_monster:
+                if "attribute" in first_card:
+                    response += f"属性: {first_card['attribute']}\n"
+                if "attack" in first_card:
+                    response += f"攻击力: {first_card['attack']}\n"
+                if "defense" in first_card:
+                    response += f"防御力: {first_card['defense']}\n"
+                # 根据卡片类型显示相应的字段
+                if "link" in first_card:
+                    response += f"链接: {first_card['link']}\n"
+                elif "rank" in first_card:
+                    response += f"阶级: {first_card['rank']}\n"
+                elif "level" in first_card:
+                    response += f"等级: {first_card['level']}\n"
+            
             if "description" in first_card:
                 response += f"效果: {first_card['description']}\n"
             
@@ -200,21 +205,26 @@ class CardQueryPlugin(Star):
                 card = result["results"][0]
                 card_info = f"找到卡片：{card['name']}\n"
                 card_info += f"类型：{card['type']}\n"
-                if "attribute" in card:
-                    card_info += f"属性：{card['attribute']}\n"
-                # 根据卡片类型显示相应的字段
-                if "link" in card:
-                    card_info += f"链接：{card['link']}\n"
-                elif "rank" in card:
-                    card_info += f"阶级：{card['rank']}\n"
-                elif "level" in card:
-                    card_info += f"等级：{card['level']}\n"
-                if "race" in card:
-                    card_info += f"种族：{card['race']}\n"
-                if "attack" in card:
-                    card_info += f"攻击力：{card['attack']}\n"
-                if "defense" in card:
-                    card_info += f"防御力：{card['defense']}\n"
+                
+                # 只有怪兽卡才显示属性、攻击、防御、等级等信息
+                is_monster = "怪兽" in card.get('type', '')
+                if is_monster:
+                    if "attribute" in card:
+                        card_info += f"属性：{card['attribute']}\n"
+                    # 根据卡片类型显示相应的字段
+                    if "link" in card:
+                        card_info += f"链接：{card['link']}\n"
+                    elif "rank" in card:
+                        card_info += f"阶级：{card['rank']}\n"
+                    elif "level" in card:
+                        card_info += f"等级：{card['level']}\n"
+                    if "race" in card:
+                        card_info += f"种族：{card['race']}\n"
+                    if "attack" in card:
+                        card_info += f"攻击力：{card['attack']}\n"
+                    if "defense" in card:
+                        card_info += f"防御力：{card['defense']}\n"
+                
                 if "description" in card:
                     card_info += f"效果：{card['description']}\n"
                 card_info += "请根据以上信息回答用户问题，不要添加额外信息。"
